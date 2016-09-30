270d63ee76dcfa268dcb3b212284e435c6b98d89
ale-blanco <ablancosoft@gmail.com>
2016-09-30 18:22:47 +0200

    Añadida opcion de indicar la codificacion del sms, para poder enviar mensajes unicode.

--- index.php
+++ index.php
@@ -2,74 +2,89 @@
 
 require 'src/smsup/smsuplib.php';
 
-$s = new smsup\smsuplib('TU_ID_USUARIO','TU_CLAVE_SECRETA');
+$s = new smsup\smsuplib('TU_ID_USUARIO', 'TU_CLAVE_SECRETA');
 $resul = '';
-if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-	if($_POST['ord']=='envio'){
-		$texto = $_POST['texto'];
-		$numeros = array($_POST['tel']);
-		$referencia = $_POST['ref'];
-		$remitente = (array_key_exists('remit', $_POST))?$_POST['remit']:'';
-		$resul = json_encode($s->NuevoSMS($texto,$numeros,'', $referencia, $remitente), JSON_PRETTY_PRINT);
-	}else if($_POST['ord']=='eliminar'){
-		$resul = json_encode($s->EliminarSMS($_POST['id']), JSON_PRETTY_PRINT);
-	}else if($_POST['ord']=='estado'){
-		$resul = json_encode($s->EstadoSMS($_POST['id']), JSON_PRETTY_PRINT);
-	}else if($_POST['ord']=='creditos'){
-		$resul = json_encode($s->CreditosDisponibles(), JSON_PRETTY_PRINT);
-	}else if($_POST['ord']=='resultado'){
-		$resul = json_encode($s->ResultadoPeticion($_POST['ref']), JSON_PRETTY_PRINT);
-	}
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if ($_POST['ord']=='envio') {
+        $texto = $_POST['texto'];
+        $numeros = array($_POST['tel']);
+        $referencia = $_POST['ref'];
+        $remitente = (array_key_exists('remit', $_POST))?$_POST['remit']:'';
+        $codificacion = $_POST['cod'];
+        $resul = json_encode($s->NuevoSMS($texto, $numeros, '', $referencia, $remitente, $codificacion), JSON_PRETTY_PRINT);
+    } elseif ($_POST['ord']=='eliminar') {
+        $resul = json_encode($s->EliminarSMS($_POST['id']), JSON_PRETTY_PRINT);
+    } elseif ($_POST['ord']=='estado') {
+        $resul = json_encode($s->EstadoSMS($_POST['id']), JSON_PRETTY_PRINT);
+    } elseif ($_POST['ord']=='creditos') {
+        $resul = json_encode($s->CreditosDisponibles(), JSON_PRETTY_PRINT);
+    } elseif ($_POST['ord']=='resultado') {
+        $resul = json_encode($s->ResultadoPeticion($_POST['ref']), JSON_PRETTY_PRINT);
+    }
 
-	echo '<pre>'.$resul.'</pre>';
+    echo '<pre>'.$resul.'</pre>';
 }
 
 ?>
 
-<form method="POST">
-	<h3>Enviar sms</h3>
-	<input type="hidden" name="ord" value="envio" />
-	Texto:<br>
-	<textarea maxlenght="160" name="texto" cols="50" rows="10"></textarea>
-	<br>Telefono:<br>
-	<input type="text" name="tel" />
-	<br>Referencia:<br>
-	<input type="text" name="ref" />
-	<br>Remitente:<br>
-	<input type="text" name="remit" />
-	<br>
-	<input type="submit" value="Enviar sms" />
-</form>
-<br>
-<form method="POST">
-	<h3>Estado sms</h3>
-	<input type="hidden" name="ord" value="estado" />
-	Id sms obtenido del envio:<br>
-	<input type="text" name="id" />
-	<br>
-	<input type="submit" value="Ver estado" />
-</form>
-<br>
-<form method="POST">
-	<h3>Eliminar sms</h3>
-	<input type="hidden" name="ord" value="eliminar" />
-	Id sms obtenido del envio:<br>
-	<input type="text" name="id" />
-	<br>
-	<input type="submit" value="Eliminar sms" />
-</form>
-<br>
-<form method="POST">
-	<h3>Resultado envio anterior</h3>
-	<input type="hidden" name="ord" value="resultado" />
-	Referencia de la peticion:<br>
-	<input type="text" name="ref" />
-	<br>
-	<input type="submit" value="Ver resultado" />
-</form>
-<br>
-<form method="POST">
-	<h3>Creditos disponibles</h3>
-	<input type="hidden" name="ord" value="creditos" />
-	<input type="submit" value="Ver creditos" />
-</form>
+
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+    </head>
+    <body>
+        <form method="POST">
+            <h3>Enviar sms</h3>
+            <input type="hidden" name="ord" value="envio" />
+            Texto:<br>
+            <textarea maxlenght="160" name="texto" cols="50" rows="10"></textarea>
+            <br>Telefono:<br>
+            <input type="text" name="tel" />
+            <br>Referencia:<br>
+            <input type="text" name="ref" />
+            <br>Remitente:<br>
+            <input type="text" name="remit" />
+            <br>Codificacion:<br>
+            <select name="cod">
+                <option value="<?php echo smsup\smsuplib::GSM ?>">Gsm</option>
+                <option value="<?php echo smsup\smsuplib::UNICODE ?>">Unicode</option>
+                <option value="<?php echo smsup\smsuplib::AUTO ?>">Auto</option>
+            </select>
+            <br>
+            <input type="submit" value="Enviar sms" />
+        </form>
+        <br>
+        <form method="POST">
+            <h3>Estado sms</h3>
+            <input type="hidden" name="ord" value="estado" />
+            Id sms obtenido del envio:<br>
+            <input type="text" name="id" />
+            <br>
+            <input type="submit" value="Ver estado" />
+        </form>
+        <br>
+        <form method="POST">
+            <h3>Eliminar sms</h3>
+            <input type="hidden" name="ord" value="eliminar" />
+            Id sms obtenido del envio:<br>
+            <input type="text" name="id" />
+            <br>
+            <input type="submit" value="Eliminar sms" />
+        </form>
+        <br>
+        <form method="POST">
+            <h3>Resultado envio anterior</h3>
+            <input type="hidden" name="ord" value="resultado" />
+            Referencia de la peticion:<br>
+            <input type="text" name="ref" />
+            <br>
+            <input type="submit" value="Ver resultado" />
+        </form>
+        <br>
+        <form method="POST">
+            <h3>Creditos disponibles</h3>
+            <input type="hidden" name="ord" value="creditos" />
+            <input type="submit" value="Ver creditos" />
+        </form>
+    </body>
+</html>
--- src/smsup/smsuplib.php
+++ src/smsup/smsuplib.php
@@ -2,92 +2,108 @@
 
 namespace smsup;
 
-class smsuplib {
+class smsuplib
+{
+    const GSM = 'gsm';
+    const UNICODE = 'uni';
+    const AUTO = 'aut';
 
-	const HOST = 'https://www.smsup.es';
-	const URL_SMS = '/api/sms/';
-	const URL_CREDITOS = '/api/creditos/';
-	const URL_ENVIO = '/api/peticion/';
+    const HOST = 'https://www.smsup.es';
+    const URL_SMS = '/api/sms/';
+    const URL_CREDITOS = '/api/creditos/';
+    const URL_ENVIO = '/api/peticion/';
 
-	protected $usuario = '';
-	protected $clave = '';
+    protected $usuario = '';
+    protected $clave = '';
 
-	public function __construct ($usuario,$clave){
-		$this->usuario = $usuario;
-		$this->clave = $clave;
-	}
+    public function __construct($usuario, $clave)
+    {
+        $this->usuario = $usuario;
+        $this->clave = $clave;
+    }
 
-	public function NuevoSMS($texto, Array $numeros, $fechaenvio=null, $referencia='', $remitente=''){
-		$fechac = (($fechaenvio===null || !is_object($fechaenvio))?'NOW':$fechaenvio->format('c'));
-		$post = json_encode(array('texto'=>$texto, 'fecha' => $fechac,
-			'telefonos' => $numeros, 'referencia'=>$referencia, 'remitente'=> $remitente));
-		$cabeceras = $this->generarCabeceras('POST', self::URL_SMS, $post);
-		return $this->enviar(self::URL_SMS, 'POST', $cabeceras, $post);
-	}
+    public function NuevoSMS($texto, array $numeros, $fechaenvio = null, $referencia = '', $remitente = '', $codificacion = null)
+    {
+        $fechac = (($fechaenvio===null || !is_object($fechaenvio))?'NOW':$fechaenvio->format('c'));
+        $post = ['texto'=>$texto, 'fecha' => $fechac,
+            'telefonos' => $numeros, 'referencia'=>$referencia, 'remitente'=> $remitente];
+        if ($codificacion!=null && in_array($codificacion, [self::GSM, self::UNICODE, self::AUTO])) {
+            $post['codificacion'] = $codificacion;
+        }
+        $post = json_encode($post);
+        $cabeceras = $this->generarCabeceras('POST', self::URL_SMS, $post);
+        return $this->enviar(self::URL_SMS, 'POST', $cabeceras, $post);
+    }
 
-	public function EliminarSMS($idsms){
-		$url = self::URL_SMS.$idsms.'/';
-		$cabeceras = $this->generarCabeceras('DELETE', $url);
-		return $this->enviar($url, 'DELETE', $cabeceras);
-	}
+    public function EliminarSMS($idsms)
+    {
+        $url = self::URL_SMS.$idsms.'/';
+        $cabeceras = $this->generarCabeceras('DELETE', $url);
+        return $this->enviar($url, 'DELETE', $cabeceras);
+    }
 
-	public function EstadoSMS($idsms){
-		$url = self::URL_SMS.$idsms.'/';
-		$cabeceras = $this->generarCabeceras('GET', $url);
-		return $this->enviar($url, 'GET', $cabeceras);
-	}
+    public function EstadoSMS($idsms)
+    {
+        $url = self::URL_SMS.$idsms.'/';
+        $cabeceras = $this->generarCabeceras('GET', $url);
+        return $this->enviar($url, 'GET', $cabeceras);
+    }
 
-	public function CreditosDisponibles(){
-		$cabeceras = $this->generarCabeceras('GET', self::URL_CREDITOS);
-		return $this->enviar(self::URL_CREDITOS, 'GET', $cabeceras);
-	}
+    public function CreditosDisponibles()
+    {
+        $cabeceras = $this->generarCabeceras('GET', self::URL_CREDITOS);
+        return $this->enviar(self::URL_CREDITOS, 'GET', $cabeceras);
+    }
 
-	public function ResultadoPeticion($referencia){
-		$url = self::URL_ENVIO.$referencia.'/';
-		$cabeceras = $this->generarCabeceras('GET', $url);
-		return $this->enviar($url, 'GET', $cabeceras);
-	}
+    public function ResultadoPeticion($referencia)
+    {
+        $url = self::URL_ENVIO.$referencia.'/';
+        $cabeceras = $this->generarCabeceras('GET', $url);
+        return $this->enviar($url, 'GET', $cabeceras);
+    }
 
 
 
-	protected function generarCabeceras($verbo, $url, $post=''){
-		$cabeceras = array();
-		$smsdate = date('c');
-		$cabeceras[] = 'Sms-Date: '.$smsdate;
-		$text = $verbo.$url.$smsdate.$post;
-		$firma = hash_hmac('sha1', $text, $this->clave);
-		$cabeceras[] = 'Firma: '.$this->usuario.':'.$firma;
-		return $cabeceras;
-	}
+    protected function generarCabeceras($verbo, $url, $post = '')
+    {
+        $cabeceras = array();
+        $smsdate = date('c');
+        $cabeceras[] = 'Sms-Date: '.$smsdate;
+        $text = $verbo.$url.$smsdate.$post;
+        $firma = hash_hmac('sha1', $text, $this->clave);
+        $cabeceras[] = 'Firma: '.$this->usuario.':'.$firma;
+        return $cabeceras;
+    }
 
-	protected function enviar($url, $method, $cabeceras, $body = null){
-		$url = self::HOST.$url;
-		$ch = curl_init($url);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		if($method == "POST") {
-			curl_setopt($ch, CURLOPT_POST, true);
-		}else if($method=='DELETE'){
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
-		}
-		curl_setopt($ch, CURLOPT_HTTPHEADER, array_values($cabeceras));
-		if($body !== null) {
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
-		}
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-		$data = curl_exec($ch);
-		if (curl_errno($ch)) {
-			throw new \Exception(curl_error($ch));
-		} else {
-			$statusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-		}
-		curl_close($ch);
-		if ($data) {
-			$result = json_decode($data,true);
-			if ($result !== false) {
-				return array('httpcode'=>$statusCode, 'resultado' => $result);
-			}
-		}
+    protected function enviar($url, $method, $cabeceras, $body = null)
+    {
+        $url = self::HOST.$url;
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        if ($method == "POST") {
+            curl_setopt($ch, CURLOPT_POST, true);
+        } elseif ($method=='DELETE') {
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
+        }
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array_values($cabeceras));
+        if ($body !== null) {
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
+        }
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $data = curl_exec($ch);
+        if (curl_errno($ch)) {
+            throw new \Exception(curl_error($ch));
+        } else {
+            $statusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        }
+        curl_close($ch);
+        if ($data) {
+            $result = json_decode($data, true);
+            if ($result !== false) {
+                return array('httpcode'=>$statusCode, 'resultado' => $result);
+            }
+        }
 
-		return false;
-	}
+        return false;
+    }
 }
